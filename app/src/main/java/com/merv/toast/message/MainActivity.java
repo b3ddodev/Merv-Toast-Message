82511e3a89f44a86a57db6dbf60bded09e748ed3
b3ddo dEV <withb3ddodev@gmail.com>
2025-02-07 21:09:51 +0300

    📁 Add Merv Toast Message Sample App

--- app/src/main/java/com/merv/toast/message/MainActivity.java
+++ app/src/main/java/com/merv/toast/message/MainActivity.java
@@ -1,11 +1,8 @@
 package com.merv.toast.message;
 
 import android.os.Bundle;
-
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.google.android.material.button.MaterialButton;
-import com.merv.toast.library.MervToastMessage;
 
 public class MainActivity extends AppCompatActivity {
 
